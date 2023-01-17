9a31648e732fc97fead051b834c7526e7a60cec1
Olivier3456 <olivier.larrey@gmail.com>
2023-01-17 21:08:50 +0100

    Dancing animations finished

--- Assets/CreativeCore_Animation/Scripts/DanceParty.cs
+++ Assets/CreativeCore_Animation/Scripts/DanceParty.cs
@@ -5,46 +5,30 @@ using UnityEngine;
 public class DanceParty : MonoBehaviour
 {
     private Animator anim;
-    
-
-    // Start is called before the first frame update
+        
     void Start()
     {
-        anim = GetComponent<Animator>();
-       
+        anim = GetComponent<Animator>();       
     }
-
-    // Update is called once per frame
+        
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
+        if (Input.GetKeyDown(KeyCode.Q))     
             anim.SetTrigger("Dance_01");
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
+        
+        if (Input.GetKeyDown(KeyCode.W))        
             anim.SetTrigger("Dance_02");
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
+        
+        if (Input.GetKeyDown(KeyCode.E))        
             anim.SetTrigger("Dance_03");
-        }
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
+        
+        if (Input.GetKeyDown(KeyCode.R))        
             anim.SetTrigger("Dance_04");
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
+        
+        if (Input.GetKeyDown(KeyCode.T))        
             anim.SetTrigger("Dance_05");
-        }
-
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            anim.SetTrigger("Dance_06");
-        }
+        
+        if (Input.GetKeyDown(KeyCode.Y))        
+            anim.SetTrigger("Dance_06");        
     }
 }
